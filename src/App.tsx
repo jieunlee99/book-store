dd924e01c3b67833acde75320679d185b3629a74
jieunlee99 <lll3346@naver.com>
2024-07-25 18:03:35 +0900

    week15-day3

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import Layout from "./components/layout/Layout";
 import Home from "./pages/Home";
-import ThemeSwitcher from "./components/header/ThemeSwitcher";
 import { BookStoreThemeProvider } from "./context/themeContext";
 
 function App() {
   return (
     <>
       <BookStoreThemeProvider>
-        <ThemeSwitcher />
         <Layout>
           <Home />
         </Layout>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,17 @@
+import Button from "../components/common/Button";
+import InputText from "../components/common/InputText";
+import Title from "../components/common/Title";
+
 function Home() {
   return (
     <>
+      <Title size="medium" color="background">
+        제목 텍스트
+      </Title>
+      <Button size="large" scheme="primary">
+        버튼 텍스트
+      </Button>
+      <InputText placeholder="여기에 입력하세요"/>
       <div>home body</div>
     </>
   );
